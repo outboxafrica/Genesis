1a6503b5f2c343d11cf82fca6a29cacc897dcd65
Karenolivia13 <karenolivem13@gmail.com>
2020-10-02 13:58:29 +0300

    log in functionality

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+//variables
+const logInButton = document.getElementById('btn-log-in');
+const logInForm = document.getElementById('logIn');
+const usernamelogIn = document.getElementById('username').value;
+const passwordlogIn = document.getElementById('password').value;
+
+
+//event listeners
+logInForm.addEventListener('submit', (e)=>{
+e.preventDefault();
+logInEvent();
+});
+
+
+//functions
+function logInEvent (){
+    
+}
