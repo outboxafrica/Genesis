b8f7a163b7ebf5641c9c98d535fb52dbe817589f
Karenolivia13 <karenolivem13@gmail.com>
2020-10-05 15:03:32 +0300

    merging sign up page with sign up page origin

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,31 @@
 //Variables -- MOST VARIABLES SHOULD BE DECLARED HERE
 const signUpForm = document.getElementById('signup-form');
+
+
+
+
 //Sign up form Validation -- TO BE DONE BY HANNAH
+//     // get the modal
+// let modal = document.getElementById("modal-form")
+
+// // get the button that opens the modal
+// let btn = document.getElementById("modal-button")
+
+// // get the span element that closes the modal
+// let span = document.querySelector(".close")
+
+// // when the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block"
+// }
+
+// // when the user clicks on the close button, close the modal
+// span.onclick = function() {
+//   modal.style.display = "none"
+// }
+
+
+
 
 
 //Implementing Browser storage on sign up form
