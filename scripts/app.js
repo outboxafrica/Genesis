4d829baebe3352433582f2d57794796f36bd5cf0
Karenolivia13 <karenolivem13@gmail.com>
2020-10-06 11:30:29 +0300

    log in page

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,18 @@
-//variables
+//Variables -- MOST VARIABLES SHOULD BE DECLARED HERE
+
 const logInButton = document.getElementById('btn-log-in');
 const logInForm = document.getElementById('logIn');
 const usernamelogIn = document.getElementById('username').value;
 const passwordlogIn = document.getElementById('password').value;
+const signUpForm = document.getElementById('signup-form');
 
 
-//event listeners
+//Event listeners
 logInForm.addEventListener('submit', (e)=>{
 e.preventDefault();
 logInEvent();
 });
 
-
-//functions
-function logInEvent (){
-    
-}
-//Variables -- MOST VARIABLES SHOULD BE DECLARED HERE
-const signUpForm = document.getElementById('signup-form');
-//Sign up form Validation -- TO BE DONE BY HANNAH
 
 
 //Implementing Browser storage on sign up form
@@ -31,24 +25,28 @@ signUpForm.addEventListener('submit', (e)=>{
         var usernameInput = document.getElementById('Username').value;
         var passwordInput = document.getElementById('psw').value;
         var idInput = document.getElementById('id').value;
+        var emailInput = document.getElementById('email').value;
 
         localStorage.setItem("username", (usernameInput));
         localStorage.setItem("password", (passwordInput));
         localStorage.setItem("id", (idInput));
-        
+        localStorage.setItem("email", (emailInput));
+
         
         
         // Retrieve
         let usernameCheck = localStorage.getItem("username");
         let passwordCheck = localStorage.getItem("password");
         let idCheck = localStorage.getItem("id");
+        let emailCheck = localStorage.getItem("email");
 
          
          
         console.log(usernameCheck);
         console.log(passwordCheck);
         console.log(idCheck);
-        
+        console.log(emailCheck);
+       
         
         } else {
         console.log("Sorry, your browser does not support Web Storage...");
@@ -56,6 +54,17 @@ signUpForm.addEventListener('submit', (e)=>{
 });
 
 
+
+//Functions
+function logInEvent (){
+    
+}
+
+
+
+
+
+//Sign up form Validation -- TO BE DONE BY HANNAH
 
 
 //Create the different functionality for the three different users
