069b389b1c5f97a13a5dcca9195914ecb4cbfe53
Karenolivia13 <karenolivem13@gmail.com>
2020-10-09 14:06:05 +0300

    mini-cart added

--- scripts/app.js
+++ scripts/app.js
@@ -60,9 +60,22 @@ signUpForm.addEventListener('submit', (e)=>{
 function logInEvent (){
     
 }
-
-
-
+document.addEventListener('DOMContentLoaded', function(){
+const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', (e) => {
+    if(e.target.tagName == 'LI'){
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      Array.from(panels).forEach((panel) => {
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
+        }
+      });
+    }
+  });
+})
 
 
 function openCategory(event, category) {
