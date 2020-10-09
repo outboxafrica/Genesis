a17c4e5a574f30ed5a3c2bde9648370f12bb04f9
Karenolivia13 <karenolivem13@gmail.com>
2020-10-10 01:22:51 +0300

    ft-local storage and validation added

--- scripts/app.js
+++ scripts/app.js
@@ -1,194 +1,161 @@
 //Variables -- MOST VARIABLES SHOULD BE DECLARED HERE
-const signUpForm = document.getElementById('signup-form');
+const signUpForm = document.getElementById('signupform');
 const logInButton = document.getElementById('btn-log-in');
 const logInForm = document.getElementById('logIn');
-// const usernamelogIn = document.getElementById('username').value;
-// const passwordlogIn = document.getElementById('password').value;
 
+//Functions
 // //Signup form validation
-// const form = document.getElementById('form');
-// const username = document.getElementById('Username');
-// const email = document.getElementById('email');
-// const id = document.getElementById('id');
-// const password = document.getElementById('psw');
-// const password2 = document.getElementById('psw2');
 
-// form.addEventListener('submit', e => {
-// 	e.preventDefault();
+function userValid(){
+  var pat2=/(?=.*[ A-Za-z0-9_@./#&+-])(?=.{5,})/;
+  var userpat=pat2.exec(signupform.Username.value);
+  if(userpat==null){
+      document.getElementById('err1').textContent = "Username should contain at least one number or symbol and uppercase letter, it can't be less than 5 characters";
+  }
+  else{
+      document.getElementById('err1').textContent="";
+      document.getElementById('err1').style.display = "none";
+
+  }
+}
+function emailValid(){
+  var pat5 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var emailpat = pat5.exec(signupform.email.value);
+  if(emailpat==null){
+    document.getElementById('err1').textContent="Incorrect email format";
+
+  }
+  else{
+    document.getElementById('err1').textContent="";
+    document.getElementById('err1').style.display = "none";
+
+  }
+}
+function passValid(){
+  var pat3=/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+  var passPat=pat3.exec(signupform.psw.value);
+  if(passPat==null){
+    document.getElementById('err2').innerHTML ="Password should contain at least one number, symbol, uppercase and lowercase letter";
+    // document.getElementById('err2').style.display = "block";
+  
+  }
+  else{
+      document.getElementById('err2').innerHTML="";
+      document.getElementById('err2').style.display = "none";
+  }
+}
+function passMatch(){
+  var pass1=signupform.psw.value;
+  var pass2=signupform.cpsw.value;
+  if(pass1!=pass2){
+      document.getElementById('err3').textContent="Passwords do not match!";
+  }
+  else{
+      document.getElementById('err3').textContent="";
+      document.getElementById('err3').style.display = "none";
+
+  }
+}
+function idValid(){
+  var pat4 = /^[0-9]{5}$/;
+  var idpat=pat4.exec(signup-form.id.value);
+  if(idpat==null){
+      document.getElementById('err4').textContent="ID contains numbers only";
+  }
+  else{
+      document.getElementById('err4').textContent="";
+  }
+}
+function myID() {
+  var select = document.getElementById("register").value;
+ var idInput = document.getElementById("id");
+if(select == 'EDU Staff'){
+   idInput.setAttribute('maxlength', '1');
+  }
+ else if(select == 'EDU LF'){
+ idInput.setAttribute('maxlength', '2');
+ }
+ else if(select == 'EDU student'){
+ idInput.setAttribute('maxlength', '3');
+ }
+}
+
+
+
 	
-// 	checkInputs();
-// });
-
-// function checkInputs() {
-// 	// trim to remove the whitespaces
-// 	const usernameValue = username.value.trim();
-//     const emailValue = email.value.trim();
-//     const id = id.value.trim();
-// 	const passwordValue = psw.value.trim();
-// 	const password2Value = psw2.value.trim();
-	
-// 	if(usernameValue === '') {
-// 		setErrorFor(username, 'Username cannot be blank');
-// 	} else {
-// 		setSuccessFor(username);
-// 	}
-	
-// 	if(emailValue === '') {
-// 		setErrorFor(email, 'Email cannot be blank');
-// 	} else if (!isEmail(emailValue)) {
-// 		setErrorFor(email, 'Not a valid email');
-// 	} else {
-// 		setSuccessFor(email);
-// 	}
-	
-// 	if(passwordValue === '') {
-// 		setErrorFor(password, 'Password cannot be blank');
-// 	} else {
-// 		setSuccessFor(password);
-// 	}
-	
-// 	if(password2Value === '') {
-// 		setErrorFor(password2, 'Password2 cannot be blank');
-// 	} else if(passwordValue !== password2Value) {
-// 		setErrorFor(password2, 'Passwords does not match');
-// 	} else{
-// 		setSuccessFor(password2);
-// 	}
-// }
-
-// function setErrorFor(input, message) {
-// 	const formControl = input.parentElement;
-// 	const small = formControl.querySelector('small');
-// 	formControl.className = 'form-group error';
-// 	small.innerText = message;
-// }
-
-// function setSuccessFor(input) {
-// 	const formControl = input.parentElement;
-// 	formControl.className = 'form-group success';
-// }
-	
-// function isEmail(email) {
-// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-// }
-
-
-//Sign up form Validation -- TO BE DONE BY HANNAH
-
-
-
-
-
-
-
-
-
-
 
 //Event listeners
-logInForm.addEventListener('submit', (e)=>{
-e.preventDefault();
-logInEvent();
-});
 
 
 
-//Implementing Browser storage on sign up form
-
-signUpForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    // Check browser support for local storage
-    if (typeof(Storage) !== "undefined") {
-        // Store data
-
-       
-        const message = "Form draft has been saved!";
-        displayAlert(message);
-
-
-        var usernameInput = document.getElementById('Username').value;
-        var passwordInput = document.getElementById('psw').value;
-        var idInput = document.getElementById('id').value;
-        var emailInput = document.getElementById('email').value;
-
-        localStorage.setItem("username", (usernameInput));
-        localStorage.setItem("password", (passwordInput));
-        localStorage.setItem("id", (idInput));
-        localStorage.setItem("email", (emailInput));
-
-        
-        
-        // // Retrieve
-        // let usernameCheck = localStorage.getItem("username");
-        // let passwordCheck = localStorage.getItem("password");
-        // let idCheck = localStorage.getItem("id");
-        // let emailCheck = localStorage.getItem("email");
-
-         
-         
-        // console.log(usernameCheck);
-        // console.log(passwordCheck);
-        // console.log(idCheck);
-        // console.log(emailCheck);
-       
-        
-        } else {
-        console.log("Sorry, your browser does not support Web Storage...");
-        }
-});
 
 // saveCheckbox.addEventListener('checked', ()=>{
 //   document.onload = populateForm(); // populate the form when the document is loaded
 // });
 
 
-//Functions
-function logInEvent (){
-    
-}
 
+
+let message = "Account created!";
+const alertBox = document.querySelector('.alert');
 const displayAlert = (message) => {
   alertBox.innerText = message; // add the message into the alert box
   alertBox.style.display = "block"; // make the alert box visible
+  alertBox.style.backgroundColor = 'green'; 
+  alertBox.style.color = 'white'; 
+  
   setTimeout(function() {
-    alertBox.style.display = "none"; // hide the alert box after 1 second
-  }, 1000);
+    alertBox.style.display = "none"; // hide the alert box after 2 second
+  }, 2000);
 };
 
 
 
-function openCategory(event, category) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(category).style.display = "flex";
-    event.currentTarget.className += " active";
-  }
-
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
-
-
-
-  //Sign up form Validation -- TO BE DONE BY HANNAH
 
 //Create the different functionality for the three different users
 
 
 //Authenticity for the log in form
  //A loop may be needed to loop over the data in the cache and return a match 
- 
+
+ //Implementing Browser storage on sign up form
+
+        let userData = [];
+        // example {id:1592304983049, title: 'Deadpool', year: 2015}
+        const addUser = ()=>{
+             //to stop the form submitting
+            let user = {
+                id: document.getElementById('id').value,
+                title: document.getElementById('register').value,
+                uname: document.getElementById('Username').value
+            }
+            userData.push(user);
+            // document.forms[0].reset(); // to clear the form for the next entries
+
+            //for display purposes only
+            console.warn('added' , {userData} );
+            
+            //saving to localStorage
+            if(user.id !== "" || user.title !== "" || user.uname !== ""){
+              localStorage.setItem('Users', JSON.stringify(userData) );
+
+            }
+            document.querySelector('#signup-form').reset();
+            
+            displayAlert(message);
+        }
+        
+  document.addEventListener('DOMContentLoaded', ()=>{
+            // document.getElementById('signupbtn').addEventListener('click', (e)=>{
+            //   e.preventDefault();
+              
+            // });
+            signUpForm.addEventListener('submit', (e)=>{
+              e.preventDefault();
+              addUser()
+          });
+          
+        });
  
 
