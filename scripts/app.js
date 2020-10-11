61e92720da9a5596cb27270aa967df40010dec19
Cynthia-me <ayecythia@gmail.com>
2020-10-11 10:04:39 +0300

    support page,JS not responding

--- scripts/app.js
+++ scripts/app.js
@@ -99,22 +99,17 @@ function openCategory(event, category) {
  //A loop may be needed to loop over the data in the cache and return a match 
  
 //FOR SUPPORT PAGE
-var slideIndex = 0;
-showSlides();
+var myIndex = 0;
+carousel();
 
-function showSlides() {
+function carousel() {
   var i;
-  var slides = document.getElementsByClassName("myslide");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
