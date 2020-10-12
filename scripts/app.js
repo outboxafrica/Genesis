169393103f59310f8806c5b33201bb2ee2f3e4e9
Scovia Achan <achanscovia@gmail.com>
2020-10-12 10:39:23 +0300

    books

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 // const signUpForm = document.getElementById('signupform');
 // const logInButton = document.getElementById('btn-log-in');
 // const logInForm = document.getElementById('logIn');
-// const borrowBtn = document.querySelector('.borrow');
+const borrowBtn = document.querySelector('.borrow');
 const list = document.querySelector('.all-books ul');
 
 
@@ -22,83 +22,83 @@ const list = document.querySelector('.all-books ul');
 //   }
 // }
 
-//  //Signup form validation
+ //Signup form validation
 
-// function userValid() {
-//   var pat2 = /(?=.*[ A-Za-z0-9_@./#&+-])(?=.{5,})/;
-//   var userpat = pat2.exec(signupform.Username.value);
-//   if (userpat == null) {
-//     document.getElementById('err1').textContent = "Username should contain at least one number or symbol and uppercase letter, it can't be less than 5 characters";
-//   }
-//   else {
-//     document.getElementById('err1').textContent = "";
-//     document.getElementById('err1').style.display = "none";
+function userValid() {
+  var pat2 = /(?=.*[ A-Za-z0-9_@./#&+-])(?=.{5,})/;
+  var userpat = pat2.exec(signupform.Username.value);
+  if (userpat == null) {
+    document.getElementById('err1').textContent = "Username should contain at least one number or symbol and uppercase letter, it can't be less than 5 characters";
+  }
+  else {
+    document.getElementById('err1').textContent = "";
+    document.getElementById('err1').style.display = "none";
 
-//   }
-// }
-// function emailValid() {
-//   var pat5 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-//   var emailpat = pat5.exec(signupform.email.value);
-//   if (emailpat == null) {
-//     document.getElementById('err1').textContent = "Incorrect email format";
+  }
+}
+function emailValid() {
+  var pat5 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var emailpat = pat5.exec(signupform.email.value);
+  if (emailpat == null) {
+    document.getElementById('err1').textContent = "Incorrect email format";
 
-//   }
-//   else {
-//     document.getElementById('err1').textContent = "";
-//     document.getElementById('err1').style.display = "none";
+  }
+  else {
+    document.getElementById('err1').textContent = "";
+    document.getElementById('err1').style.display = "none";
 
-//   }
-// }
+  }
+}
 
-// function passValid() {
-//   var pat3 = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
-//   var passPat = pat3.exec(signupform.psw.value);
-//   if (passPat == null) {
-//     document.getElementById('err2').innerHTML = "Password should contain at least one number, symbol, uppercase and lowercase letter";
-//     // document.getElementById('err2').style.display = "block";
+function passValid() {
+  var pat3 = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+  var passPat = pat3.exec(signupform.psw.value);
+  if (passPat == null) {
+    document.getElementById('err2').innerHTML = "Password should contain at least one number, symbol, uppercase and lowercase letter";
+    // document.getElementById('err2').style.display = "block";
 
-//   }
-//   else {
-//     document.getElementById('err2').innerHTML = "";
-//     document.getElementById('err2').style.display = "none";
-//   }
-// }
-// function passMatch() {
-//   var pass1 = signupform.psw.value;
-//   var pass2 = signupform.cpsw.value;
-//   if (pass1 != pass2) {
-//     document.getElementById('err3').textContent = "Passwords do not match!";
-//   }
-//   else {
-//     document.getElementById('err3').textContent = "";
-//     document.getElementById('err3').style.display = "none";
+  }
+  else {
+    document.getElementById('err2').innerHTML = "";
+    document.getElementById('err2').style.display = "none";
+  }
+}
+function passMatch() {
+  var pass1 = signupform.psw.value;
+  var pass2 = signupform.cpsw.value;
+  if (pass1 != pass2) {
+    document.getElementById('err3').textContent = "Passwords do not match!";
+  }
+  else {
+    document.getElementById('err3').textContent = "";
+    document.getElementById('err3').style.display = "none";
 
-//   }
-// }
-// function idValid() {
-//   var pat4 = /^[0-9]{1,4}$/;
-//   var idpat = pat4.exec(signupform.id.value);
-//   if (idpat == null) {
-//     document.getElementById('err4').textContent = "ID contains numbers only";
-//   }
-//   else {
-//     document.getElementById('err4').textContent = "";
-//   }
-// }
+  }
+}
+function idValid() {
+  var pat4 = /^[0-9]{1,4}$/;
+  var idpat = pat4.exec(signupform.id.value);
+  if (idpat == null) {
+    document.getElementById('err4').textContent = "ID contains numbers only";
+  }
+  else {
+    document.getElementById('err4').textContent = "";
+  }
+}
 
-// function myID() {
-//   var select = document.getElementById("register").value;
-//   var idInput = document.getElementById("id");
-//   if (select == 'EDU Staff') {
-//     idInput.setAttribute('maxlength', '1');
-//   }
-//   else if (select == 'EDU LF') {
-//     idInput.setAttribute('maxlength', '2');
-//   }
-//   else if (select == 'EDU student') {
-//     idInput.setAttribute('maxlength', '3');
-//   }
-// }
+function myID() {
+  var select = document.getElementById("register").value;
+  var idInput = document.getElementById("id");
+  if (select == 'EDU Staff') {
+    idInput.setAttribute('maxlength', '1');
+  }
+  else if (select == 'EDU LF') {
+    idInput.setAttribute('maxlength', '2');
+  }
+  else if (select == 'EDU student') {
+    idInput.setAttribute('maxlength', '3');
+  }
+}
 
 
 // const tabs = document.querySelector('.tabs');
@@ -116,31 +116,31 @@ const list = document.querySelector('.all-books ul');
 //   }
 // });
 
-function openCategory(event, category) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+// function openCategory(event, category) {
+//   // Declare all variables
+//   var i, tabcontent, tablinks;
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
 
-  }
+//   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(category).style.display = "flex"; 
-  // document.getElementById(category).style.visibility = "visible"; 
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(category).style.display = "flex"; 
+//   // document.getElementById(category).style.visibility = "visible"; 
 
-  event.currentTarget.className += " active";
-}
+//   event.currentTarget.className += " active";
+// }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
 
 // filter books
 const forms = document.forms;
